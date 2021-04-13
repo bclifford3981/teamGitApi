f8d9e3c1d824107ba8675eb989acb87199cc1581
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-04-13 11:10:35 -0400

    data/services/models

---
+++ Archer.Data/Archer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archer.Data
+{
+    public enum FieldType { Field, Office, Lab}
+    public class Archer
+    {
+        [Required]
+        public int AgentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Key]
+        public Guid DbId { get; set; }
+        public int MyProperty { get; set; }
+        public FieldType FieldType { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+
+    }
+}
